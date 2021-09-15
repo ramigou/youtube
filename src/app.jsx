360504350648f7eccbf097237ff4206927073002
ramigou <dasul1021@gmail.com>
2021-09-15 22:11:14 +0900

    detail & list or grid UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 import styles from "./app.module.css";
 import SearcHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
 // App 컴포넌트에서 network 객체를 생성하면 계속 새로 생성되므로 index.js에서 props로 넘겨줌
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
+    setSelectedVideo(null);
     youtube
       .search(query) //
-      .then((videos) => setVideos(videos));
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   // 빈 배열을 전달면 처음 mount 될 때만 호출 됨
@@ -23,7 +33,23 @@ function App({ youtube }) {
   return (
     <div className={styles.app}>
       <SearcHeader onSearch={search} />
-      <VideoList videos={videos} />
+      {/* 상세 페이지는 왼쪽, 리스트는 오른쪽에 위치하기 위한 UI */}
+      <section className={styles.content}>
+        {/* 선택된 비디오가 있다면 리스트 오른쪽에 아니면 그대로 */}
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        {/* 컴포넌트에 직접 class를 지정할 수 없으므로 부모 태그로 감싸야함 */}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -3,20 +3,29 @@ import styles from "./video_item.module.css";
 
 // API로 받아온 동영상의 제목을 브라우저에 출력
 // props.video.snippet 중복되므로 deconstructing 해서 사용하는 방법도 있음
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt={`${snippet.title}_thumbnail`}
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+// onVideoClick 함수의 인자로 video 사용하기 위해서 snippet으로 받아온 것 외에 따로 또 받아옴
+const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
+  const displayType = display === "list" ? styles.list : styles.grid;
+
+  return (
+    // 전달받은 display 타입에 따라 css를 적용하기 위함
+    <li
+      className={`${styles.container} ${displayType}`}
+      onClick={() => onVideoClick(video)}
+    >
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt={`${snippet.title}_thumbnail`}
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-);
+    </li>
+  );
+};
 
 export default VideoItem;
