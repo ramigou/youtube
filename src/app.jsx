6cc5c7c86960f98d8a35cfd38b4ff6bb7bf867f4
ramigou <dasul1021@gmail.com>
2021-09-15 18:11:08 +0900

    make search UI

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from "react";
-import "./app.css";
+import styles from "./app.module.css";
+import SearcHeader from "./components/search_header/search_header";
 import VideoList from "./components/video_list/video_list";
 
 function App() {
   const apikey = process.env.REACT_APP_Youtube_API_KEY;
   const [videos, setVideos] = useState([]);
+
+  // 검색 API 호출
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow"
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=30&q=${query}&type=video&key=${apikey}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  };
 
   // 빈 배열을 전달면 처음 mount 될 때만 호출 됨
   useEffect(() => {
@@ -24,7 +44,13 @@ function App() {
       .then((result) => setVideos(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-  return <VideoList videos={videos} />;
+
+  return (
+    <div className={styles.app}>
+      <SearcHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
